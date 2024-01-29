c41b77b064f011954e2d151f85bfa13d2e5e0ef2
paulo1306 <paulo1306@usp.br>
2024-01-29 18:37:38 -0300

    Deleted unnecessary files

--- rmd.py
+++ rmd.py
@@ -36,32 +36,39 @@ class RMDCell:
 
         self.soma.cm = 1.630574514 # (uF/cm2) (Nicoletti et. al)
         #Insert custom currents defined in .mod files
-        #self.soma.insert('Kv3') OK
-        #self.soma.insert('Kv4') OK 
-        #self.soma.insert('Kir') OK 
-        #self.soma.insert('Cav1') OK
+        self.soma.insert('Kv3') 
+        self.soma.insert('Kv4')  
+        self.soma.insert('Kir')  
+        self.soma.insert('Cav1') 
         self.soma.insert('Cav2')  
-        #self.soma.insert('Cav3') OK
+        self.soma.insert('Cav3')
 
-        #self.soma.g_Kv3_bar_Kv3   = .00176646 #S/cm2 (egl-36)
-        #self.soma.gl_Kv4          = .00054352 #S/cm2 (leak) 
-        #self.soma.g_Nalcn_Kv4          = .00006794 #S/cm2 (nca)
-        #self.soma.g_Kv4_bar_Kv4   = .00339703 #S/cm2 (shl-1)  
-        #self.soma.g_Kir_bar_Kir   = .00027176 #S/cm2 (irkl-3) 
-        #self.soma.g_Cav1_bar_Cav1 = .00134522 #S/cm2 (elg-19) 
+        self.soma.g_Kv3_bar_Kv3   = .00176646 #S/cm2 (egl-36)
+        self.soma.gl_Kv4          = .00054352 #S/cm2 (leak) 
+        self.soma.g_Nalcn_Kv4          = .00006794 #S/cm2 (nca)
+        self.soma.g_Kv4_bar_Kv4   = .00339703 #S/cm2 (shl-1)  
+        self.soma.g_Kir_bar_Kir   = .00027176 #S/cm2 (irkl-3) 
+        self.soma.g_Cav1_bar_Cav1 = .00134522 #S/cm2 (elg-19) 
         self.soma.g_Cav2_bar_Cav2 = .00122293 #S/cm2 (unc-2) 
-        #self.soma.g_Cav3_bar_Cav3 = .00421232 #S/cm2 (cca-1)
+        self.soma.g_Cav3_bar_Cav3 = .00421232 #S/cm2 (cca-1)
 
         # Insert passive current in the dendrite
         #self.dend.insert('pas')
         #self.dend.g_pas = 0.001  # Passive conductance in S/cm2
         #self.dend.e_pas = -80    # Leak reversal potential mV
                 
-    def add_current_stim(self, delay):
-        self.stim = h.IClamp(self.soma(0.5)) #1.0 is the distal end of the dendrite
-        self.stim.amp = 0.004 # input current in nA
-        self.stim.delay = delay  # turn on after this time in ms
-        self.stim.dur = 50  # duration of 1 ms
+    def add_current_stim1(self, delay,curr):
+        self.stim1 = h.IClamp(self.soma(0.5)) #1.0 is the distal end of the dendrite
+        self.stim1.amp = curr*0.001 # input current in nA
+        self.stim1.delay = delay  # turn on after this time in ms
+        self.stim1.dur = 25  # duration of 1 ms
+    
+    def add_current_stim2(self, delay,curr):
+        self.stim2 = h.IClamp(self.soma(0.5)) #1.0 is the distal end of the dendrite
+        self.stim2.amp = curr*0.001 # input current in nA
+        self.stim2.delay = delay  # turn on after this time in ms
+        self.stim2.dur = 25  # duration of 1 ms
+    
     
     def set_recording(self):
         """Set soma, dendrite, and time recording vectors on the cell. """
@@ -89,9 +96,13 @@ class RMDCell:
 rmdCell = RMDCell()
 rmdCell.set_recording()
 
-rmdCell.add_current_stim(900)
+rmdCell.add_current_stim1(900,5)
 
-h.tstop = 1500  # (ms) set simulation duration
+
+rmdCell.add_current_stim2(1400,-10)
+
+
+h.tstop = 2000  # (ms) set simulation duration
 h.init()
 h.run()  # run simulation
 
